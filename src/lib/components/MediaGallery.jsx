cad3128255d2e4f8cf6c09ee0eba7f4f4b033880
Pushkar Dixit <dixitpushkar28@gmail.com>
2026-05-24 05:35:29 +0530

    feat: enhance media gallery to conditionally render sections for images, audio, and other media

--- src/lib/components/MediaGallery.jsx
+++ src/lib/components/MediaGallery.jsx
@@ -73,61 +73,73 @@ function MediaGallery({ rootFolder }) {
       {/* Image */}
 
       <div className="p-20 min-h-lvh flex gap-20 flex-col">
-        <div className="flex justify-center flex-col  gap-10">
-          <h2 className="flex justify-center text-2xl">{"Images"}</h2>
-          {/* Images */}
-          <div className=" grid grid-cols-3 gap-2 justify-center items-center">
-            {image.map((item, index) => (
-              <img
-                key={`img-${index}`}
-                src={URL.createObjectURL(item.file)}
-                alt={item.name}
-                className="w-full h-36 object-cover rounded flex justify-center items-center border"
-              />
-            ))}
+        {image.length > 0 ? (
+          <div className="flex justify-center flex-col  gap-10">
+            <h2 className="flex justify-center text-2xl">{"Images"}</h2>
+            {/* Images */}
+            <div className=" grid grid-cols-3 gap-2 justify-center items-center">
+              {image.map((item, index) => (
+                <img
+                  key={`img-${index}`}
+                  src={URL.createObjectURL(item.file)}
+                  alt={item.name}
+                  className="w-full h-36 object-cover rounded flex justify-center items-center border"
+                />
+              ))}
+            </div>
           </div>
-        </div>
+        ) : (
+          ""
+        )}
         {/* Audio */}
-        <div className="flex justify-center flex-col  gap-10">
-          <h2 className="flex justify-center text-2xl">{"Audio"}</h2>
-          <div className=" grid grid-cols-3 gap-2 justify-center items-center">
-            {audio.map((item, index) => (
-              <div
-                key={`audio-${index}`}
-                className="border p-2 rounded gap-5 flex flex-col"
-                title={item.name}
-              >
-                <p className="w-full h-24 p-10 text-wrap object-cover rounded flex justify-center items-center border-4 border-black">
-                  {item.name.length > 30
-                    ? item.name.substring(0, 60) + "..."
-                    : item.name}
-                </p>
+        {audio.length > 0 ? (
+          <div className="flex justify-center flex-col  gap-10">
+            <h2 className="flex justify-center text-2xl">{"Audio"}</h2>
+            <div className=" grid grid-cols-3 gap-2 justify-center items-center">
+              {audio.map((item, index) => (
+                <div
+                  key={`audio-${index}`}
+                  className="border p-2 rounded gap-5 flex flex-col"
+                  title={item.name}
+                >
+                  <p className="w-full h-24 p-10 text-wrap object-cover rounded flex justify-center items-center border-4 border-black">
+                    {item.name.length > 30
+                      ? item.name.substring(0, 60) + "..."
+                      : item.name}
+                  </p>
 
-                <audio controls className="w-full">
-                  <source
-                    src={URL.createObjectURL(item.file)}
-                    type={item.file.type}
-                  />
-                </audio>
+                  <audio controls className="w-full">
+                    <source
+                      src={URL.createObjectURL(item.file)}
+                      type={item.file.type}
+                    />
+                  </audio>
+                </div>
+              ))}
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {media.length > 0 ? (
+          <div className="flex justify-center flex-col  gap-10">
+            <h2 className="flex justify-center text-2xl">{"Media"}</h2>
+            {/* Other Media */}
+            {media.map((item, index) => (
+              <div
+                key={`media-${index}`}
+                className="border p-3 rounded flex items-center gap-2"
+              >
+                <Icon icon="material-symbols:attach-file" width="20" />
+
+                <span className="text-sm">{item.name}</span>
               </div>
             ))}
           </div>
-        </div>
-
-        <div className="flex justify-center flex-col  gap-10">
-          <h2 className="flex justify-center text-2xl">{"Media"}</h2>
-          {/* Other Media */}
-          {media.map((item, index) => (
-            <div
-              key={`media-${index}`}
-              className="border p-3 rounded flex items-center gap-2"
-            >
-              <Icon icon="material-symbols:attach-file" width="20" />
-
-              <span className="text-sm">{item.name}</span>
-            </div>
-          ))}
-        </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
